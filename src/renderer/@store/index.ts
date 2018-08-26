73bf5246a322b9592d6e43e64ca088e4ec60c0b7
Atif Afzal <atif5801@gmail.com>
2018-08-26 22:47:11 +0530

    setup redux hmr

--- src/renderer/@store/index.ts
+++ src/renderer/@store/index.ts
@@ -1,6 +1,8 @@
 import { applyMiddleware, createStore, Middleware, Store } from 'redux';
 import { createLogger } from 'redux-logger';
 import { createEpicMiddleware } from 'redux-observable';
+import { BehaviorSubject } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 import { IRootAction, IRootState, rootEpic, rootReducer } from '@reducers';
 
@@ -26,8 +28,25 @@ const configureStore = (): Store<IRootState, IRootAction> => {
 
 const store = configureStore();
 
+const epic$ = new BehaviorSubject(rootEpic);
+// Every time a new epic is given to epic$ it
+// will unsubscribe from the previous one then
+// call and subscribe to the new one because of
+// how switchMap works
+const hotReloadingEpic = (...args: any[]) =>
+  epic$.pipe(switchMap(epic => epic(args[0], args[1], args[2])));
+
+epicMiddleware.run(hotReloadingEpic);
+
 if (process.env.NODE_ENV !== 'production') {
   (window as any).store = store;
+
+  if (module.hot) {
+    module.hot.accept('@reducers', () => {
+      store.replaceReducer(require('@reducers').rootReducer);
+      epic$.next(require('@reducers').rootEpic);
+    });
+  }
 }
 
 export default store;
