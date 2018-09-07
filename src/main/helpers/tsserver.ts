9e6018d509381fb001e251c4167b4513247521f4
Atif Afzal <atif5801@gmail.com>
2018-09-08 00:02:22 +0530

    Add definition example

--- src/main/helpers/tsserver.ts
+++ src/main/helpers/tsserver.ts
@@ -64,15 +64,27 @@ export default startServer;
 
 if (!module.parent) {
   // tslint:disable-next-line: no-console
-  const { write } = startServer(console.log);
+  const { write } = startServer(x => console.dir(x, { depth: null }));
 
   write({
     seq: 1,
     type: 'request',
-    command: 'open',
+    command: protocol.CommandTypes.Open,
     arguments: {
       file:
         '/Users/atifafzal/scratch/personal/icarus/workspace/src/components/PlayControl/index.tsx',
+    },
+  });
+
+  write({
+    seq: 19,
+    type: 'request',
+    command: protocol.CommandTypes.Definition,
+    arguments: {
+      file:
+        '/Users/atifafzal/scratch/personal/icarus/workspace/src/components/PlayControl/index.tsx',
+      line: 41,
+      offset: 6,
     },
   });
 }
