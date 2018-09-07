451cec6a9e4666883ff7e5967f8240f22847ea20
Atif Afzal <atif5801@gmail.com>
2018-09-07 22:35:54 +0530

    add tsserver

--- src/main/helpers/tsserver.ts
+++ src/main/helpers/tsserver.ts
@@ -1,26 +1,78 @@
 import { spawn } from 'child_process';
 import * as path from 'path';
+import * as R from 'ramda';
+import * as protocol from 'typescript/lib/protocol';
 
 const tsserverPath = path.resolve(
   __dirname,
-  '../../ui/node_modules/typescript/lib/tsserver.js',
+  '../../../workspace/node_modules/typescript/lib/tsserver.js',
 );
 
-function startServer() {
+const safeParse = (body: string) => {
+  try {
+    return JSON.parse(body);
+  } catch {
+    return;
+  }
+};
+
+type ServerMessage = protocol.Response | protocol.Event | protocol.Request;
+
+function startServer(
+  onMessage: (msg: ServerMessage) => void,
+  onError?: (error: string) => void,
+) {
   const process = spawn('node', [tsserverPath]);
 
   process.stdout.on('data', data => {
-    // tslint:disable-next-line:no-console
-    console.log(data.toString());
-    process.stdin.write(
-      '{"seq":1,"type":"quickinfo","command":"open","arguments":{"file":"/Users/atifafzal/scratch/personal/meta-x/ui/src/workspace/components/Form/index.tsx"}}\n',
-    );
+    const messages: ServerMessage[] = R.pipe(
+      R.toString,
+      R.split(/[\r\n]+/g),
+      R.reduce(
+        (results, line) => {
+          const msg: ServerMessage = safeParse(line);
+
+          if (msg) {
+            results.push(msg);
+          }
+
+          return results;
+        },
+        [] as ServerMessage[],
+      ),
+    )(data);
+
+    messages.forEach(message => onMessage(message));
   });
 
   process.stderr.on('data', data => {
-    // tslint:disable-next-line:no-console
-    console.error(data.toString());
+    if (onError) {
+      onError(data.toString());
+    }
   });
+
+  function write<T extends protocol.Request>(msg: T) {
+    process.stdin.write(JSON.stringify(msg) + '\n');
+  }
+
+  return {
+    write,
+  };
 }
 
 export default startServer;
+
+if (!module.parent) {
+  // tslint:disable-next-line: no-console
+  const { write } = startServer(console.log);
+
+  write({
+    seq: 1,
+    type: 'request',
+    command: 'open',
+    arguments: {
+      file:
+        '/Users/atifafzal/scratch/personal/icarus/workspace/src/components/PlayControl/index.tsx',
+    },
+  });
+}
